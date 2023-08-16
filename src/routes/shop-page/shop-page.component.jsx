cf91082c56202f5c34f0f8ccb1c82efd0783f869
woodylucas <wlucas0311@gmail.com>
2023-08-16 08:51:26 -0400

    feat: created product component

--- src/routes/shop-page/shop-page.component.jsx
+++ src/routes/shop-page/shop-page.component.jsx
@@ -1,15 +1,16 @@
 import { useContext } from "react";
+import ProductCard from "../../components/product-card/product-card.components";
+
+import "./shop-page.styles.scss";
 
 import { ProductsContext } from "../../contexts/products.context";
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
-      {products.map(({ id, name }) => (
-        <div key={id}>
-          <h1>{name}</h1>
-        </div>
+    <div className="products-container">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
