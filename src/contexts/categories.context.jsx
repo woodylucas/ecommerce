f8c015e16c6df71e93f734b64e6157841a2b19c8
woodylucas <wlucas0311@gmail.com>
2023-08-18 21:57:03 -0400

    feat: removed unused import

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -2,8 +2,6 @@ import { createContext, useMemo, useState, useEffect } from "react";
 
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
 
-import SHOP_DATA from "../shop-data";
-
 export const CategoriesContext = createContext({
   categoriesMap: {},
   setCategories: () => null,
