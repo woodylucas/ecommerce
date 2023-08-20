ea35bac4c37aa0519f4c51eed8cd9b8f3563577e
woodylucas <wlucas0311@gmail.com>
2023-08-20 09:00:19 -0400

    feat: demistifying loggers

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,4 +1,5 @@
 export const selectCategoriesMap = (state) => {
+  console.log("selector fired");
   const categoriesMap = state.categories.categories.reduce((acc, category) => {
     const { title, items } = category;
     acc[title.toLowerCase()] = items;
--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,26 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
-const middlwares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+  // Middleware entry point
+  if (!action.type) {
+    return next(action); // If the action doesn't have a type, continue with the next middleware
+  }
 
-const composedEhancers = compose(applyMiddleware(...middlwares));
+  // Log information before processing the action
+  console.log("type: ", action.type);
+  console.log("payload: ", action.payload);
+  console.log("currentState: ", store.getState());
+
+  // Continue with the next middleware or reducer
+  next(action);
+
+  // Log information after processing the action
+  console.log("next state: ", store.getState());
+};
+
+const middlewares = [loggerMiddleware];
+
+const composedEhancers = compose(applyMiddleware(...middlewares));
 
 export const store = createStore(rootReducer, undefined, composedEhancers);
