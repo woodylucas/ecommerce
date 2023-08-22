78ec2e1d9051ec6860a89e02de897d06a453876d
woodylucas <wlucas0311@gmail.com>
2023-08-22 19:48:02 -0400

    fixed: spinner

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,10 +1,10 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const fetchCategoriesStart = () =>
-  createAction(createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
 
 export const fetchCategoriesSuccess = (categoriesArray) =>
   createAction(
