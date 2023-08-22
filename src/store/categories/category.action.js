4e602ac7d902b9c535fc012e68c19669b6249753
woodylucas <wlucas0311@gmail.com>
2023-08-22 19:07:15 -0400

    refactored: refactored async operations to leverage redux-thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,26 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
-export const setCategories = (categories) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+export const fetchCategoriesStart = () =>
+  createAction(createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoriesArray = await getCategoriesAndDocuments("categories");
+    dispatch(fetchCategoriesSuccess(categoriesArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
