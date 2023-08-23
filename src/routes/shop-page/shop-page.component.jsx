e46b909c3bced2c36cca5f61eb073dc0a7d23d4b
woodylucas <wlucas0311@gmail.com>
2023-08-23 17:07:43 -0400

    feat: integrated function generators for async operations

--- src/routes/shop-page/shop-page.component.jsx
+++ src/routes/shop-page/shop-page.component.jsx
@@ -4,13 +4,13 @@ import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories.component";
 import Category from "../category/category.component";
-import { fetchCategoriesStartAsync } from "../../store/categories/category.action";
+import { fetchCategoriesStart } from "../../store/categories/category.action";
 
 const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchCategoriesStartAsync());
+    dispatch(fetchCategoriesStart());
   }, []);
 
   return (
